45d90cad313fc78ec3137c19bf8c307f74f995fe
Bryan <b.velez205@gmail.com>
2023-07-22 13:15:27 -0400

    updates

--- software-engineering-portfolio/src/components/Resume.jsx
+++ software-engineering-portfolio/src/components/Resume.jsx
@@ -1,86 +1,93 @@
-
-
 const Resume = () => {
   return (
     <div className="resume-content">
-     
-        <a
-          href="file:///Users/bryanvelez/Downloads/Official%20Resume%202023.pdf"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Click here to view the PDF
-        </a>
-        <h1>Resume</h1>
-        <h3>Bryan Velez</h3>
-        <h4>Software Engineer</h4>
-        <p>
-          Dedicated software engineer with a passion for creating innovative
-          designs that align with consumer demand in finding solutions and
-          creating products that are sustainable over time. I have a background
-          in meeting customer and employer needs under pressure and am driven by
-          a passion for expanding my knowledge and skills. I thrive on solving
-          complex coding challenges with creative thinking and problem-solving
-          skills. My goal is to apply my creativity to front-end development and
-          video game design, focusing on enhancing user interfaces through
-          interactive, seamless, and responsive features and services. I am
-          committed to improving user experiences through approachable
-          navigation in websites and software, ensuring inclusivity and
-          accessibility for all. Moreover, I aim to provide user-friendly and
-          effective results by incorporating functionality and aesthetics.
-        </p>
-        <h4>Skills and Expertise</h4>
-        <p>
-          HTML | CSS | JavaScript | Node.js | Express | Github | MongoDB |
-          Mongoose | React | Python | Django | SQL
-        </p>
-        <h4>Technical Project Experience</h4>
-        <p>Battleship Live Site  - Front-end web browser game of the two-player classic board game Battleship
-Created a structured HTML file to set up a layout of the page.
-Used DOM manipulation methods to select and interact with elements in the HTML
-Insert Javascript code to provide transitions, pulls and animations within the game
-Created elements and arrays within Javascript to facilitate dry code and store and process data.
-Managed the use of Event listeners to add drag and drop and rotation functionality to the player ships as well as interaction with the enemy game board.
-Employed Object-Oriented-Programing principles by defining a class to create objects with specific properties.
-Manipulated CSS classes to control the appearance and behavior of HTML elements.
-Applied use of conditional statements to maneuver the flow of execution based on certain conditions. (valid ship placement, winning and losing conditions.)
-
-</p>
-<p>
-
-The Libation Station Live Site  -  React application that organizes and cocktail API information on backend, and displays ingredients and recipes on the front end.
-Pair programmed with 2 software developers effectively, avoiding conflicts by applying version control and working in our own branches, regular communication, and task delegation to build a fully functional React application..
-Imported and implemented necessary dependencies from different libraries, including React, axios, and react-router-dom.
-Integrated 3rd party API call by performing an asynchronous operation to fetch information for when the components mount.
-Engineered routes that render different components based on the current URL path.
-Utilized hooks to enable functional components to have state management, perform side effects, access location data, and retrieve URL parameters.
-Implemented ES6 tagged template literals with Styled-Components along CSS3 to provide style and flexbox layout.
-</p>
-        <p>Battleship - Web browser game</p>
-        <h4>Experience</h4>
-        <p>
-          Boston Beer Garden - Server/ Bartender October 2021-February 2023
-          Demonstrated Comprehensive product knowledge encompassing the menu,
-          specials and events. Fostered positive relationships with customers,
-          resulting in heightened satisfaction and enhanced loyalty to the
-          restaurant. Contributed to the training of new hires, providing
-          necessary guidance and valuable insight into understanding company
-          policies, procedures, and in-depth knowledge of food and beverage
-          offerings.
-        </p>
-        <p>
-          Securitas Security Company - Security Officer October 2019-September
-          2021 Maintained community security through vigilant monitoring of
-          activities, directing the movement of residents and guests, and
-          ensuring a secure environment. Safeguarded the neighborhood by
-          conducting regular patrols to check for any signs of intrusion,
-          damage, or potential threats, while also ensuring all properties were
-          properly secured. Implemented a systematic and organized data logging
-          process to record and register visitor, resident, and vehicle
-          information, contributing to an efficient tracking system.
-        </p>
-        <h4>Education</h4>
-  
+      <a
+        href="file:///Users/bryanvelez/Downloads/Official%20Resume%202023.pdf"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Click here to view the PDF
+      </a>
+      <h1>Resume</h1>
+      <h3>Bryan Velez</h3>
+      <h4>Software Engineer</h4>
+      <p>
+        Dedicated software engineer with a passion for creating innovative
+        designs that align with consumer demand in finding solutions and
+        creating products that are sustainable over time. I have a background in
+        meeting customer and employer needs under pressure and am driven by a
+        passion for expanding my knowledge and skills. I thrive on solving
+        complex coding challenges with creative thinking and problem-solving
+        skills. My goal is to apply my creativity to front-end development and
+        video game design, focusing on enhancing user interfaces through
+        interactive, seamless, and responsive features and services. I am
+        committed to improving user experiences through approachable navigation
+        in websites and software, ensuring inclusivity and accessibility for
+        all. Moreover, I aim to provide user-friendly and effective results by
+        incorporating functionality and aesthetics.
+      </p>
+      <h4>Skills and Expertise</h4>
+      <p>
+        HTML | CSS | JavaScript | Node.js | Express | Github | MongoDB |
+        Mongoose | React | Python | Django | SQL
+      </p>
+      <h4>Technical Project Experience</h4>
+      <p>
+        Battleship Live Site - Front-end web browser game of the two-player
+        classic board game Battleship Created a structured HTML file to set up a
+        layout of the page. Used DOM manipulation methods to select and interact
+        with elements in the HTML Insert Javascript code to provide transitions,
+        pulls and animations within the game Created elements and arrays within
+        Javascript to facilitate dry code and store and process data. Managed
+        the use of Event listeners to add drag and drop and rotation
+        functionality to the player ships as well as interaction with the enemy
+        game board. Employed Object-Oriented-Programing principles by defining a
+        class to create objects with specific properties. Manipulated CSS
+        classes to control the appearance and behavior of HTML elements. Applied
+        use of conditional statements to maneuver the flow of execution based on
+        certain conditions. (valid ship placement, winning and losing
+        conditions.)
+      </p>
+      <p>
+        The Libation Station Live Site - React application that organizes and
+        cocktail API information on backend, and displays ingredients and
+        recipes on the front end. Pair programmed with 2 software developers
+        effectively, avoiding conflicts by applying version control and working
+        in our own branches, regular communication, and task delegation to build
+        a fully functional React application.. Imported and implemented
+        necessary dependencies from different libraries, including React, axios,
+        and react-router-dom. Integrated 3rd party API call by performing an
+        asynchronous operation to fetch information for when the components
+        mount. Engineered routes that render different components based on the
+        current URL path. Utilized hooks to enable functional components to have
+        state management, perform side effects, access location data, and
+        retrieve URL parameters. Implemented ES6 tagged template literals with
+        Styled-Components along CSS3 to provide style and flexbox layout.
+      </p>
+      <p>Battleship - Web browser game</p>
+      <h4>Experience</h4>
+      <p>
+        Boston Beer Garden - Server/ Bartender October 2021-February 2023
+        Demonstrated Comprehensive product knowledge encompassing the menu,
+        specials and events. Fostered positive relationships with customers,
+        resulting in heightened satisfaction and enhanced loyalty to the
+        restaurant. Contributed to the training of new hires, providing
+        necessary guidance and valuable insight into understanding company
+        policies, procedures, and in-depth knowledge of food and beverage
+        offerings.
+      </p>
+      <p>
+        Securitas Security Company - Security Officer October 2019-September
+        2021 Maintained community security through vigilant monitoring of
+        activities, directing the movement of residents and guests, and ensuring
+        a secure environment. Safeguarded the neighborhood by conducting regular
+        patrols to check for any signs of intrusion, damage, or potential
+        threats, while also ensuring all properties were properly secured.
+        Implemented a systematic and organized data logging process to record
+        and register visitor, resident, and vehicle information, contributing to
+        an efficient tracking system.
+      </p>
+      <h4>Education</h4>
     </div>
   );
 };
