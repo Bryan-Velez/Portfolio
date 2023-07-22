7f32c39f2b671cfbdf53f4fd94f1842938f04c7a
Bryan <b.velez205@gmail.com>
2023-07-22 12:15:42 -0400

    resume updates

--- software-engineering-portfolio/src/components/Resume.jsx
+++ software-engineering-portfolio/src/components/Resume.jsx
@@ -1,20 +1,24 @@
+import { Container } from '@mui/material'
+
+
 const Resume = () => {
     return (
         <div className="resume-content">
-            <a href="https://github.com/Bryan-Velez/Portfolio/blob/main/software-engineering-portfolio/src/assets/Official%20Resume%202023.pdf" target="_blank" rel="noopener noreferrer">
-    Click here to view the PDF
-  </a>
-            <h1>Resume</h1>
-            <h3>Bryan Velez</h3>
-            <h4>Software Engineer</h4>
-            <p>I'm a dedicated and driven software engineer with a passion for expanding my knowledge and skills, and a background in meeting customer and employer needs under pressure. I thrive on solving complex coding challenges with creative thinking and problem-solving skills. I aim to bring my creativity to front-end development and video game design. My focus is on enhancing user interfaces through interactive, seamless, and responsive features and services. I'm committed to improving user experiences through approachable navigation in websites and software, ensuring inclusivity and accessibility for all. As well as, providing user-friendly and effective results by incorporating functionality and aesthetics.</p>
-            <h4>Skills and Expertise</h4>
-            <p>HTML | CSS | JavaScript | Node.js | Express | Github | MongoDB | Mongoose | React | Python | Django | SQL</p>
-            <h4>Technical Project Experience</h4>
-            <p>Battleship - Web browser game</p>
-            <h4>Experience</h4>
-            <h4>Education</h4>
-
+            <Container>
+                <a href="file:///Users/bryanvelez/Downloads/Official%20Resume%202023.pdf" target="_blank" rel="noopener noreferrer">
+        Click here to view the PDF
+    </a>
+                <h1>Resume</h1>
+                <h3>Bryan Velez</h3>
+                <h4>Software Engineer</h4>
+                <p>Dedicated software engineer with a passion for creating innovative designs that align with consumer demand in finding solutions and creating products that are sustainable over time. I have a background in meeting customer and employer needs under pressure and am driven by a passion for expanding my knowledge and skills. I thrive on solving complex coding challenges with creative thinking and problem-solving skills. My goal is to apply my creativity to front-end development and video game design, focusing on enhancing user interfaces through interactive, seamless, and responsive features and services. I am committed to improving user experiences through approachable navigation in websites and software, ensuring inclusivity and accessibility for all. Moreover, I aim to provide user-friendly and effective results by incorporating functionality and aesthetics.</p>
+                <h4>Skills and Expertise</h4>
+                <p>HTML | CSS | JavaScript | Node.js | Express | Github | MongoDB | Mongoose | React | Python | Django | SQL</p>
+                <h4>Technical Project Experience</h4>
+                <p>Battleship - Web browser game</p>
+                <h4>Experience</h4>
+                <h4>Education</h4>
+            </Container>
         </div>
     )
 }
