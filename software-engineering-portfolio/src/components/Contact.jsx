c1b33e7add417d8551e7ab18c306fe87cb22c3b3
Bryan <b.velez205@gmail.com>
2023-07-23 19:29:01 -0400

    updates

--- software-engineering-portfolio/src/components/Contact.jsx
+++ software-engineering-portfolio/src/components/Contact.jsx
@@ -24,7 +24,7 @@ const Contact = () => {
         <Link to={"https://discord.gg/5chAwJqQ8v"} target="_blank" rel="noopener noreferrer">
           
           <img
-            src="https://github.com/Bryan-Velez/Portfolio/blob/main/software-engineering-portfolio/src/assets/Mail%20Icon.png?raw=true"
+            src="https://github.com/Bryan-Velez/Portfolio/blob/main/software-engineering-portfolio/src/assets/Discord%20Icon.png?raw=true"
             alt="B.velez205@gmail.com"
             style={{width:'100px'}}
           />
